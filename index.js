f948cc2233731a5ddcda2aff146157c1d76ae4e8
sam <sam.spokowski@gmail.com>
2021-03-16 17:52:47 -0500

    more doc attempts

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const port = process.env.PORT || 3000;
 //set up docs
 const pathToSwaggerUi = swaggerUi.absolutePath();
 app.use(express.static(pathToSwaggerUi));
-app.use(express.static(path.join(path.resolve(), './docs/myRetailProductServiceApi.json')));
 app.use('/docs', express.static(path.join(path.resolve(), './docs/index.html')));
+app.use('docs/myRetailProductServiceApi', express.static(path.join(path.resolve(), './docs/myRetailProductServiceApi.json')));
 
 //set up request body parsing
 app.use(express.json()) 
